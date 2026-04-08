fe5ff46ca067d6ee30c419f20f7a1d0bd9ac3612
thanhnm777@gmail.com <thanhnm777@gmail.com>
2026-04-08 03:37:29 +0000

    Add Docker Compose configuration, OpenSearch autoscale tester, and requirements file

---
+++ test1.sh
@@ -0,0 +1,10 @@
+python3 os_autoscale_tester_v2.py \
+  --scheme https \
+  --host 127.0.0.1 \
+  --port 9200 \
+  --index web_access_logs \
+  --user admin \
+  --password "P12SasasA312@3123" \
+  --batch-size 2000 \
+  --delay 0.1 \
+  --log-file opensearch_test.log
